68b6d39b5a207f5f9fbf98d64b9aea8a40b78f45
if210 <fikpi@voliacable.com>
2017-10-31 22:16:40 +0200

    errors

--- src/course/codingBat/Main.java
+++ src/course/codingBat/Main.java
@@ -1,22 +1,8 @@
-package codingBat;
+package course.codingBat;
 
-
-import codingBat.warmUp1.IcyHot;
-import codingBat.warmUp1.StartHi;
-import codingBat.warmUp1.WarmUp1;
 
 public class Main {
     public static void main(String[] args) {
-
-        StartHi st = new StartHi();
-        System.out.println(st.startHi("hi asd"));
-
-        IcyHot icyHot = new IcyHot();
-        System.out.println(icyHot.icyHot(-1, 120));
-        System.out.println(icyHot.icyHot(120, -1));
-        System.out.println(icyHot.icyHot(-1, 99));
-        System.out.println(icyHot.icyHot(99, -1));
-        System.out.println(icyHot.icyHot(120, 120));
     }
 
 
