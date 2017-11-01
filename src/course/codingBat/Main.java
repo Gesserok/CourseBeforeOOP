02bd4c8e404f287abe9cb3ded16b127ed51f45cd
Gesserok <fikpi@voliacable.com>
2017-11-01 09:14:31 +0200

    ART-CODE HomeWork before OOP Codingbat

--- src/course/codingBat/Main.java
+++ src/course/codingBat/Main.java
@@ -7,14 +7,5 @@ import course.codingBat.warmUp1.StartHi;
 public class Main {
     public static void main(String[] args) {
 
-        StartHi st = new StartHi();
-        System.out.println(st.startHi("hi asd"));
-
-        IcyHot icyHot = new IcyHot();
-        System.out.println(icyHot.icyHot(-1, 120));
-        System.out.println(icyHot.icyHot(120, -1));
-        System.out.println(icyHot.icyHot(-1, 99));
-        System.out.println(icyHot.icyHot(99, -1));
-        System.out.println(icyHot.icyHot(120, 120));
     }
 }
