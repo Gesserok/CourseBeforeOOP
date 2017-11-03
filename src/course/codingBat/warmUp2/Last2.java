46628a74574f383c1fe90637d777de6daf19a4b4
anton <fikpi@voliacable.com>
2017-11-03 09:35:29 +0200

    ART-CODE HomeWork before OOP codingbat

--- src/course/codingBat/warmUp2/Last2.java
+++ src/course/codingBat/warmUp2/Last2.java
@@ -11,9 +11,24 @@ last2("axxxaaxx") → 2
  */
 
 public class Last2 {
+    public static void main(String[] args) {
+        System.out.println(last2(""));
+        System.out.println(last2("g"));
+    }
 
 
     public static int last2(String str) {
-        return 0;
+
+        int count = 0 ;
+        if (str.isEmpty()) return 0;
+        else if (str.length() < 2) return 0;
+        else if (str.length() > 1) {
+            String foundStr = str.substring(str.length()- 2, str.length() );
+            for (int i = 0; i < str.length() - 2; i++) {
+                if (str.substring(i, i + 2).equals(foundStr))
+                    count++;
+            }
+        }
+        return count;
     }
 }
